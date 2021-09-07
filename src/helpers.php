3f5579330b9a1519b1fad69658f1ae8436334b33
Daniel Sturm <d.sturm@zirkeldesign.de>
2021-09-07 08:28:32 +0200

    Add config method to helpers

--- src/helpers.php
+++ src/helpers.php
@@ -48,3 +48,30 @@ if (!function_exists('resource_path')) {
         return Roots\resource_path($path);
     }
 }
+
+if (!function_exists('config')) {
+    /**
+     * Get / set the specified configuration value.
+     *
+     * If an array is passed as the key, we will assume you want to set an array of values.
+     *
+     * @param  array|string|null  $key
+     * @param  mixed  $default
+     * @return mixed|Repository
+     *
+     * @copyright Taylor Otwell
+     * @link https://github.com/laravel/framework/blob/8.x/src/Illuminate/Foundation/helpers.php
+     */
+    function config($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('config');
+        }
+
+        if (is_array($key)) {
+            return app('config')->set($key);
+        }
+
+        return app('config')->get($key, $default);
+    }
+}
